000fc6038d6cd5e2e9138a1279a2dcc0eab968fa
Robert Wolff <ro.wolff@gmx.net>
2018-10-25 16:22:45 +0200

    Scaffold react-app and electron incl. packaging for mac and win

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            This is some st0pid test if I can get react to work with electron...
+          </p>
+          <a
+            className="App-link"
+            href="http://rawbird.net"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            It's me
+          </a>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App
