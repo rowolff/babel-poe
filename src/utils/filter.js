147ac2f93e197c8d5d3f6ba6b2a0276734926b2e
Robert Wolff <ro.wolff@gmx.net>
2018-11-17 17:51:21 +0100

    Hotfix: translate poe.trade whispers, as well as normal ones

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,10 +1,21 @@
-const messageRegex = /.+?(?:@From)\s+(?:<(.+)>\s+)?(.+)(?=.*?:.*?:):(.*)/i
+const messageRegex = /..+?(?:@From)\s+(?:<(.+)>\s+)?(.+)(?=.*?:.*?:):\s(.*)|.+?(?:@From)\s+(?:<(.+)>\s+)?(.+):\s(.*)/i
+// this shit thing accounts for whispers which have two ":" in it,
+// which is the case for poe.trade auto generated messages :/
+// it breaks as soon as thre's a third ":" in the message
+// Vlad, please please how the f*** can I do this better??
 
 const messageFilter = message => {
   const matches = messageRegex.exec(message)
 
   if (matches) {
-    const [, guild, user, message] = matches
+    let [, guild, user, message] = matches
+    const [, , , , autoGuild, autoUser, autoMessage] = matches
+
+    if (autoGuild) {
+      guild = autoGuild
+      user = autoUser
+      message = autoMessage
+    }
 
     return {
       whisper: true,
