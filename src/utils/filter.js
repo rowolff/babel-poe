9fc73c3913dd9b6504effaf31b82aa65f5c98904
Robert Wolff <ro.wolff@gmx.net>
2018-11-17 18:25:48 +0100

    Regex cleanup

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,21 +1,10 @@
-const messageRegex = /..+?(?:@From)\s+(?:<(.+)>\s+)?(.+)(?=.*?:.*?:):\s(.*)|.+?(?:@From)\s+(?:<(.+)>\s+)?(.+):\s(.*)/i
-// this shit thing accounts for whispers which have two ":" in it,
-// which is the case for poe.trade auto generated messages :/
-// it breaks as soon as thre's a third ":" in the message
-// Vlad, please please how the f*** can I do this better??
+const messageRegex = /.+?(?:@From)\s+(?:<(.+)>\s+)?(.+?):\s+(.*)/i
 
 const messageFilter = message => {
   const matches = messageRegex.exec(message)
 
   if (matches) {
     let [, guild, user, message] = matches
-    const [, , , , autoGuild, autoUser, autoMessage] = matches
-
-    if (autoGuild) {
-      guild = autoGuild
-      user = autoUser
-      message = autoMessage
-    }
 
     return {
       whisper: true,
